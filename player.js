5ff38120c9fed928fe69027df2e17b11249252be
= <=>
2023-04-29 12:45:25 -0700

    first

---
+++ player.js
@@ -0,0 +1,66 @@
+import * as THREE from "three";
+
+let c = 0
+
+export let k = 'hi my name is';
+export let position = [0, 0, 0]
+
+export function que() {
+//move.forward = 99;
+}
+
+export let move = {
+	forward: 0,
+	side: 0,
+}
+
+const geometry = new THREE.BoxGeometry(5,5,5,5);
+const material = new THREE.MeshStandardMaterial({ color: 0x8f6347 });
+export const box = new THREE.Mesh(geometry, material);
+
+function animate() {
+	box.position.x += move.side
+	// box.position.y += .1
+	box.position.z += move.forward
+
+  requestAnimationFrame(animate);
+}
+
+animate()
+
+document.addEventListener('keydown', (e) => {
+	// console.log(e.key.toLowerCase())
+	switch(e.key.toLowerCase()) {
+		case 'w':
+			move.forward = 1
+			break;
+		case 'a':
+			move.side = 1
+			break;
+		case 's':
+			move.forward = -1
+			break;
+		case 'd':
+			move.side = -1
+			break;			
+	}
+})
+
+document.addEventListener('keyup', (e) => {
+	switch(e.key.toLowerCase()) {
+		case 'w':
+			move.forward = 0
+			break;
+		case 'a':
+			move.side = 0
+			break;		
+		case 's':
+			move.forward = 0
+			break;	
+		case 'd':
+			move.side = 0
+			break;	
+	}
+})
+
+export function m() {console.log(move)} 
