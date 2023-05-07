406f598a19838ceba75029f95330b72efbb332bb
= <=>
2023-05-07 16:29:50 -0700

    timer, stats

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as player from "./components/player.js";
 import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
 import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+import Stats from "stats.js";
 import $ from "jquery";
+let {log} = console;
+
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
 
 let PI = Math.PI;
 
@@ -23,8 +29,8 @@ camera.position.set(0, 50, -100);
 
 renderer.render(scene, camera);
 
-const spaceTexture = new THREE.TextureLoader().load("space.jpeg");
-scene.background = spaceTexture;
+// const spaceTexture = new THREE.TextureLoader().load("space.jpeg");
+// scene.background = spaceTexture;
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
@@ -40,7 +46,7 @@ let material = new THREE.MeshStandardMaterial({ color: 0x383838 });
 const plane = new THREE.Mesh(geometry, material);
 plane.position.set(0, -20, 0);
 scene.add(plane);
-
+//
 geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
@@ -51,6 +57,7 @@ scene.add(player.box);
 
 //adds targets
 Array(20).fill().forEach(addStar);
+
 function addStar() {
   const geometry = new THREE.SphereGeometry(1.5);
   const material = new THREE.MeshStandardMaterial({ color: "#D22030" });
@@ -65,17 +72,35 @@ function addStar() {
 }
 
 //updates game at 60 fps
-function animate() {
+async function animate() {
+  renderer.render(scene, camera);
   requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.01;
   torus.rotation.z += 0.01;
 
-  renderer.render(scene, camera);
+  stats.update();
+
+  // await delay(1000/60)
+  // animate()
 }
 
 animate();
+
+let targets = [];
+getTargets()
+// let positions = [[0, 50, -100], [10, 100, 70], [-80, -60, -30]]
+let positions = [[0, 50, -100], [0, 50, -105], [-0, 50, -110]]
+let pc = 0;
+
+function getTargets() {
+	scene.traverse((obj) => {
+		if (obj.isMesh && obj.geometry.type == "SphereGeometry") {
+			targets.push(obj);
+		}
+	});
+}
 
 async function delay(secs) {
   return new Promise((resolve) => {
@@ -85,14 +110,22 @@ async function delay(secs) {
 let time = 0;
 
 function timeTick() {
-	
+	time++;
+	$('.timer').html(() => {
+		let m, s;
+		m = Math.floor(time / 60)
+		s = Math.floor(time % 60)
+		console.log()
+		if(s < 10)
+			s = '0' + s
+		return `${m+':'+s}`
+	})
 }
 
-let timeIV = setInterval(timeTick)
+let timeIV = setInterval(timeTick, 1000)
 
 //scuffed debug
 document.addEventListener("keydown", async (e) => {
-  // console.log(e.key.toLowerCase())
   switch (e.key.toLowerCase()) {
     case "m":
       console.log(camera.position);
@@ -107,17 +140,18 @@ document.addEventListener("keydown", async (e) => {
       location.reload();
       break;
     case "k":
-      let m = [];
-      scene.traverse((obj) => {
-        if (obj.isMesh && obj.geometry.type == "SphereGeometry") {
-          m.push(obj);
-        }
-      });
-      if (m.length) {
-        scene.remove(m[0]);
+      if (targets.length) {
+        scene.remove(targets[0]);
+        targets.splice(0, 1);
         $(".score").html("Score: " + ++score);
-        console.log(m);
       }
+      break;
+		case ".":
+			pc++
+			camera.position.set(positions[pc][0],positions[pc][1],positions[pc][2]);
+		  pc = (pc > 2) ? 0:pc; 
+			log(pc)
+			break;
   }
 });
 
@@ -139,7 +173,8 @@ document.addEventListener("click", function () {
     // console.log(mesh)
     if (mesh.geometry.type == "SphereGeometry") {
       scene.remove(mesh);
-      $(".score").html("Score: " + ++score);
+      targets.splice(0, 1);
+			$(".score").html("Score: " + ++score);
     }
     if (score >= 20) $(".score").append(" win");
   }
